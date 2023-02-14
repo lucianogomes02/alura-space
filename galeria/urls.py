69cb2bbd297a96e20c02fd2a8857a5b37327fb8b
lucianogomes02 <luciano@pontotel.com.br>
2023-02-14 01:06:02 -0300

    feat(templates/galeria/partials/_footer.html): implementa partial do footer das páginas

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from galeria.views import index, imagem
 
-urlpatterns = [path("", index, name="index"), path("imagem/", imagem, name="imagem")]
+urlpatterns = [path("", index, name="index"), path("imagem", imagem, name="imagem")]
