6bce3b37b260ee96cf6692cc7de874d44d40e871
lucianogomes02 <luciano@pontotel.com.br>
2023-02-15 01:43:49 -0300

    refactor(galeria/admin.py): configura listagem e filtragem por categoria no ListadorDeFotografias

--- galeria/admin.py
+++ galeria/admin.py
@@ -7,6 +7,8 @@ class ListadorDeFotografias(admin.ModelAdmin):
     list_display = ("id", "nome", "legenda", "foto")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_per_page = 10
 
 
 admin.site.register(Fotografia, ListadorDeFotografias)
