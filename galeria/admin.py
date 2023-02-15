5f4c103a9e67a39a754595103f8361f1675156c0
lucianogomes02 <luciano@pontotel.com.br>
2023-02-15 01:24:33 -0300

    feat(galeria/admin.py): implemeta configurações do Django-Admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from galeria.models import Fotografia
+
+
+class ListadorDeFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "foto")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+
+
+admin.site.register(Fotografia, ListadorDeFotografias)
