adf19d558a9195098baf8a0d3fbca6c0c4df1e43
lucianogomes02 <luciano@pontotel.com.br>
2023-02-17 01:35:57 -0300

    feat(usuario/views.py): implementa lógica de autenticação de usuário do Django na view login

--- usuario/views.py
+++ usuario/views.py
@@ -2,10 +2,30 @@ from django.shortcuts import render, redirect
 from usuario.forms import LoginForm, CadastroForm
 
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 def login(request):
     formulario = LoginForm()
+
+    if request.method == "POST":
+        formulario = LoginForm(request.POST)
+
+        if formulario.is_valid():
+            nome_de_login = formulario["nome_de_login"].value()
+            senha = formulario["senha"].value()
+
+            usuario = auth.authenticate(
+                request,
+                username=nome_de_login,
+                password=senha,
+            )
+
+            if usuario:
+                auth.login(request, usuario)
+                return redirect("index")
+            else:
+                return redirect("login")
     return render(request, "usuario/login.html", {"formulario": formulario})
 
 
