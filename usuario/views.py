2806f07c68fc1de3c64de5a52afd1ba9933b262e
lucianogomes02 <luciano@pontotel.com.br>
2023-02-17 01:08:19 -0300

    feat(usuario/views.py): implementa lógica de cadastrar usuário do Django na view cadastro

--- usuario/views.py
+++ usuario/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from usuario.forms import LoginForm, CadastroForm
+
+from django.contrib.auth.models import User
 
 
 def login(request):
@@ -9,4 +11,24 @@ def login(request):
 
 def cadastro(request):
     formulario = CadastroForm()
+
+    if request.method == "POST":
+        formulario = CadastroForm(request.POST)
+
+        if formulario.is_valid():
+            nome_de_cadastro = formulario["nome_de_cadastro"].value()
+            email = formulario["email"].value()
+            senha = formulario["senha"].value()
+            confirmacao_de_senha = formulario["confirmacao_de_senha"].value()
+
+            usario_ja_existe = User.objects.filter(username=nome_de_cadastro).exists()
+
+            if senha != confirmacao_de_senha or usario_ja_existe:
+                return redirect("cadastro")
+
+            usuario = User.objects.create_user(
+                username=nome_de_cadastro, email=email, password=senha
+            )
+            usuario.save()
+            return redirect("login")
     return render(request, "usuario/cadastro.html", {"formulario": formulario})
